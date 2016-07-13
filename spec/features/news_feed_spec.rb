e63057d3a0205eb07ff486406c52e19ae31a0622
Max Provin <maxprovin@outlook.com>
2016-07-13 17:11:07 +0100

    added headline titles for some news outlets to news page

--- spec/features/news_feed_spec.rb
+++ spec/features/news_feed_spec.rb
@@ -1,1 +1,10 @@
-feature 'News Feed'
+feature 'News Feed' do
+
+  scenario 'displays feeds from multiple outlets' do
+    visit '/news'
+    expect(page).to have_content("Guardian")
+    expect(page).to have_content("Daily Mail")
+    expect(page).to have_content("Financial Times")
+  end
+
+end
