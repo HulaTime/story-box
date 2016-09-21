b4df280c115bd1dd89af91d0bf21b37d5c059bfa
MaxProvin <maxprovin01@gmail.com>
2016-09-21 19:25:40 +0100

    added model test for User.sort_notes_by_id method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,16 @@
 describe User, type: :model do
 	
+	subject(:user) {described_class.new}
+	let(:note1) { double :note1, id: 1, body: 'xyz' }
+	let(:note2) { double :note2, id: 3, body: 'abc' }
+	let(:note3) { double :note3, id: 5, body: 'sdfsaf' }
+
 	it { should have_many(:articles) }
 	it { should have_many(:notes) }
+
+	it 'should have a method to sort notes' do
+		allow(user).to receive(:notes) { [note1, note3, note2] }
+		expect(user.sort_notes_by_id).to eq [note1, note2, note3]
+	end
 
 end
