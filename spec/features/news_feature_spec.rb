38b28a8554ea3d8a1aa7fda21f84753bad960897
MaxProvin <maxprovin01@gmail.com>
2016-10-01 20:41:54 +0100

    starting to set up server-api-server-client ajax requests for headlines to keep apikey secret

--- spec/features/news_feature_spec.rb
+++ spec/features/news_feature_spec.rb
@@ -1,11 +1,18 @@
 feature 'News', :vcr do
 
-	scenario 'should have a list of media sources' do
+	before do
 		sign_user_up
 		visit news_path
+	end
+
+	scenario 'should have a list of media sources' do
 		expect(page).to have_content 'Guardian'
 		expect(page).to have_content 'Financial Times'
 		expect(page).to have_content 'BBC Sport'
+	end
+
+	scenario 'each source should display latest headlines' do
+		expect(page).to have_content ''
 	end
 
 end
