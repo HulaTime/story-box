23ddc90265f16625aa57ad075ef5c36665787b3b
MaxProvin <maxprovin01@gmail.com>
2016-10-10 23:05:35 +0100

    changed/fixed test for media/news sources

--- spec/features/news_feature_spec.rb
+++ spec/features/news_feature_spec.rb
@@ -5,10 +5,10 @@ feature 'News', :vcr do
 		visit news_path
 	end
 
-	xscenario 'should have a list of media sources' do
-		expect(page).to have_content 'Guardian'
-		expect(page).to have_content 'Financial Times'
-		expect(page).to have_content 'BBC Sport'
+	scenario 'should have a list of media sources' do
+		expect(page).to have_css("img[alt*='Guardian']")
+		expect(page).to have_css("img[alt*='Financial Times']")
+		expect(page).to have_css("img[alt*='BBC Sport']")
 	end
 
 	scenario 'each source logo is a link to the source' do
@@ -17,14 +17,16 @@ feature 'News', :vcr do
 		expect(url).to eq "http://www.abc.net.au/news"
 	end
 
-	xscenario 'each source should display latest headlines' do
+	scenario 'each source should have a description' do
+		news_sources = page.all(:css, '.news-logo')
 		expect(page).to have_content ''
 	end
 
-	scenario "each source should have its logo as an image" do
-		news_sources = page.all(:css, '.news-logo')
+	xscenario "each source should have its logo as an image" do
+		news_sources = page.all(:css, '.source-info')
 		news_sources.each do |source|
-			expect(source).to have_css('img')
+			url = source.find('a')['href']
+			expect(source).to have_xpath("//img[@src=\"/public/images/#{image}\"]")
 		end
 	end
 
