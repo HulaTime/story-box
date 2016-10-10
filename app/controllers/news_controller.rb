afe963a9fd0b1a8d23da184ecc96b7f735e55fc7
MaxProvin <maxprovin01@gmail.com>
2016-10-10 22:26:13 +0100

    changed html/css for news sources to table format because it works like i want it too :)

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -2,6 +2,7 @@ class NewsController < ApplicationController
 
 	def index
 		@news_sources = HTTParty.get("https://newsapi.org/v1/sources?language=en&apiKey=#{ENV['NEWS_KEY']}")['sources']
+		
 	end
 
 end
